e57e3ea57ece9087eb5c87c222ae372ff065868b
Lester <245252159@qq.com>
2023-04-20 23:37:36 +1000

    Dockerfile done docker-compose.yml in process

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+﻿# 基于Python 3.11.2镜像构建
+FROM python:3.11.2
+
+# 设置工作目录
+WORKDIR /Django
+
+# 将requirements.txt复制到容器中
+COPY requirements.txt .
+
+# 安装依赖
+RUN pip install -r requirements.txt
+
+# 将当前目录下的所有文件复制到容器中
+COPY . .
+
+ENV WATCHPACK_POLLING=true
+
+# 开放端口
+EXPOSE 8000
+
+# 运行命令
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
+#CMD ["python"]
--- fit5120Django/settings.py
+++ fit5120Django/settings.py
@@ -80,6 +80,14 @@ DATABASES = {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'db_backend',
+    #     'USER': 'springuser',
+    #     'PASSWORD': 'ThePassword',
+    #     'HOST': 'localhost',
+    #     'PORT': '3306',
+    # }
 }
 
 
