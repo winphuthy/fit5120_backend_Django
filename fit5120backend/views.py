766fe51fd9a6eb2df205adac07a2cf3237eefa64
Lester <245252159@qq.com>
2023-04-22 03:59:58 +1000

    WordCloud function done

--- fit5120backend/views.py
+++ fit5120backend/views.py
@@ -1,14 +1,16 @@
-import os
 import json
-from rest_framework import serializers
-from wordcloud import WordCloud
 from io import BytesIO
-from .serializers import YearDataSerializer
+
 from django.http import HttpResponse, JsonResponse
-from fit5120Django import settings
-from fit5120backend.models import Yeardata
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework import serializers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from wordcloud import WordCloud
+
+from .models import Word
+from .models import Yeardata
+from .serializers import YearDataSerializer, WordsSerializer
 
 json_str = '{"Python": 150, "Java": 120, "JavaScript": 80, "Ruby": 40, "Go": 20}'
 json_str = json.loads(json_str)
@@ -21,17 +23,26 @@ text = " ".join([k for k in json_str.keys() for i in range(json_str[k])])
 def word_cloud(request, *args, **kwargs):
     if request.method == "GET":
         wordcloud = WordCloud(background_color="white", max_words=1000, contour_width=3, contour_color='steelblue')
+        # print(json_str)
+        # return Response(json_str)
         image = wordcloud.generate_from_frequencies(json_str)
-        result = wordcloudimg_to_byt(image)
+        result = wordCloudImg_to_byt(image)
         return HttpResponse(result, content_type='image/png')
-    if request.method == "POST":
-
-        image_path = os.path.join(settings.BASE_DIR, 'fit5120backend', 'static', 'TestPicture.jpg')
-        with open(image_path, 'rb') as f:
-            image_data = f.read()
-        return HttpResponse(image_data, content_type='image/png')
-    else:
-        return JsonResponse({'msg': "Wrong request type"}, status=400)
+    if request.method == 'POST':
+        json_data = json.loads(request.body)
+        word = json_data.get('word')
+        if not word:
+            return JsonResponse({'status': 'error', 'message': 'Missing word'})
+        try:
+            word_obj = Word.objects.get(word=word)
+            word_obj.increment_count()
+        except Word.DoesNotExist:
+            word_obj = Word(word=word, count=1)
+            word_obj.save()
+        wordcloud = WordCloud(background_color="white", max_words=1000, contour_width=3, contour_color='steelblue')
+        result = generate_wordcloud_by_database(wordcloud)
+        return HttpResponse(result, content_type='image/png')
+    return JsonResponse({'status': 'error', 'message': 'Invalid request method'})
 
 
 @api_view(["GET"])
@@ -42,18 +53,32 @@ def get_year_data(request, *args, **kwargs):
         return Response(serializer.data)
     except serializers.ValidationError as e:
         return JsonResponse({'msg': str(e)}, status=400)
-    except Exception as e:
+    except Exception:
         return JsonResponse({'msg': 'Server Error'}, status=500)
 
 
 @api_view(["GET"])
+@csrf_exempt
 def test(request, *args, **kwargs):
     wordcloud = WordCloud(background_color="white", max_words=1000, contour_width=3, contour_color='steelblue')
-    image = wordcloud.generate_from_frequencies(json_str)
-    result = wordcloudimg_to_byt(image)
+    result = generate_wordcloud_by_database(wordcloud)
     return HttpResponse(result, content_type='image/png')
 
-def wordcloudimg_to_byt(image):
+
+def generate_wordcloud_by_database(wordcloud):
+    words = Word.objects.all()
+    serializer = WordsSerializer(words, many=True)
+    # print(serializer.data)
+    # return Response(serializer.data)
+    reformat = {}
+    for item in serializer.data:
+        reformat[item['word']] = item['count']
+    image = wordcloud.generate_from_frequencies(reformat)
+    result = wordCloudImg_to_byt(image)
+    return result
+
+
+def wordCloudImg_to_byt(image):
     """
     Transfer wordCloud image class to byt data
     :param image: wordCloud class image
