0023b1a52b5a02141d0e8cd94e61477cb0b1c3ea
Lester <245252159@qq.com>
2023-04-25 20:49:22 +1000

    update CORS

--- fit5120Django/settings.py
+++ fit5120Django/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'corsheaders',
     'fit5120backend.apps.Fit5120BackendConfig',
 ]
 
@@ -48,7 +49,10 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = "fit5120Django.urls"
 
